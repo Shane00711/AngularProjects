b620070aae39480d5caff9cbc3efcc7b149e7b1e
sifmfe <sifiso.mfeya@thecortexhub.com>
2017-12-20 08:10:17 +0200

    Material Imports

--- pchalk/src/app/app.module.ts
+++ pchalk/src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule, MatMenuModule, MatCardModule, MatToolbarModule, MatIconModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 
@@ -10,7 +14,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
